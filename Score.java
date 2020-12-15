7a8eb1ca19a708b3b44a7cd772b1c7d9037bcceb
Redfalcon5-ai <71303491+Redfalcon5-ai@users.noreply.github.com>
2020-12-15 09:32:04 -0600

    Create Score.java

---
+++ Score.java
@@ -0,0 +1,85 @@
+import java.util.*;
+public class Score //make sure that this is a subclass of Calculations and inherits all of the properties of Calculations
+{ 
+   
+   /*Initialize all of the following instance variables 
+    You should have 14 private integers with the following names
+    onesScore, twosScore, threesScore, foursScore, fivesScore, sixesScore,
+    chanceScore, smallStraightScore, largeStraightScore, threeOfAKindScore,
+    fourOfAKindScore, fullHouseScore, yahtzeeScore, totalScore
+    All of the integers should be set to -1 except totalScore
+    totalScore should be set to 0.
+    You will also need one more instance variable that is a String named menu*/
+    
+    public String printMenu()
+    {
+        /*You should set the variable menu to an empty string.  Then you should create a menu
+        that looks like the video of the final product example however the line should only show 
+        up if it has not been scored.That is why all of the variables are set to -1 because if 
+        they have a score of 0 or above they should not be in the menu.*/
+        //This should return the String menu
+       
+    }   
+    
+    public void setOnesScore(int a)
+    {
+        //This should pass in an integer that sets the score for the onesScore   
+    }
+    public void setTwosScore(int a)
+    {
+        //This should pass in an integer that sets the score for the twosScore
+    }
+    public void setThreesScore(int a)
+    {
+        //This should pass in an integer that sets the score for the threesScore  
+    }
+    public void setFoursScore(int a)
+    {
+        //This should pass in an integer that sets the score for the foursScore
+    }
+    public void setFivesScore(int a)
+    {
+        //This should pass in an integer that sets the score for the fivesScore
+    }
+    public void setSixesScore(int a)
+    {
+        //This should pass in an integer that sets the score for the sixesScore
+    }
+    public void setChanceScore(int a)
+    {
+        //This should pass in an integer that sets the score for the chanceScore   
+    }
+    public void setSmallStraightScore(int a)
+    {
+        //This should pass in an integer that sets the score for the smallStraightScore
+    }
+    public void setLargeStraightScore(int a)
+    {
+        //This should pass in an integer that sets the score for the largeStraightScore
+    }
+    public void set3OfAKindScore(int a)
+    {
+        //This should pass in an integer that sets the score for the threeOfAKindScore
+    }
+    public void set4OfAKindScore(int a)
+    {
+        //This should pass in an integer that sets the score for the fourOfAKindScore
+    }
+    public void setFullHouseScore(int a)
+    {
+        //This should pass in an integer that sets the score for the fullHouseScore
+    }
+    public void setYahtzeeScore(int a)
+    {
+        //This should pass in an integer that sets the score for the yahtzeeScore 
+    }
+    
+    public int getScore()
+    {
+        /*This method should set up total score and set it to zero
+        it should proceed to check if each score or each variable is
+        zero or over and then add it to total score*/
+        //This should return the variable totalScore
+        
+    }
+}
