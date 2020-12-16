a207ebf2f3f921e6338ff57003cf72e82fa81a2a
creator44 <68862410+creator44@users.noreply.github.com>
2020-12-16 15:52:23 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,4 +1,4 @@
-public class Player //make sure that this is a subclass of Score and inherits all of the properties of Score
+public class Player extends Score//make sure that this is a subclass of Score and inherits all of the properties of Score
 {
     /You should make and instance variable called name/
     public String name;
