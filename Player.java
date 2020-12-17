adab01550d9469c26bceb96be9de47d35a833e08
Redfalcon5-ai <71303491+Redfalcon5-ai@users.noreply.github.com>
2020-12-17 14:02:08 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,11 +1,11 @@
 public class Player extends Score//make sure that this is a subclass of Score and inherits all of the properties of Score
 {
-    /You should make and instance variable called name/
+    //You should make and instance variable called name
     public String name;
 
     public Player(String name)
     {
-       this.name = name
+       this.name = name;
         // set the String that is passed through to the private instance variable  you created for this class
     } 
 
